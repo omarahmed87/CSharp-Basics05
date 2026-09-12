0a368e917534dce7a47df9d19194f1ce46fefe04
Omar Ahmed <cresssoomar@gmail.com>
2026-09-12 16:18:09 +0300

    14st answer

--- Basics 05/Program.cs
+++ Basics 05/Program.cs
@@ -1,6 +1,6 @@
-﻿#region 13st answer
-/*1-Add a private string password = "secret"; field to a Book class. Try to 
-print it from Main  (outside the class). What happens, and why? 
+﻿#region 14st answer
+/*2-Add an internal int copiesInStock = 5; field to Book. Print it from Main. 
+Does it compile?  Why?
 */
 #endregion
 
@@ -9,12 +9,13 @@ public class Program
     public static void Main()
     {
         Book book = new Book();
-        Console.WriteLine(book.password);
-        // It can't acces varriable password out of own class because it's private.
-        // it can access just in own class(scope).
+        Console.WriteLine(book.copiesInStock);
+        // Yes, it compiles.
+        // 'internal' members are accessible anywhere within the same assembly (project).
+
     }
     public class Book
     {
-        private string password= "secret";
+        internal int copiesInStock = 5;
     }
 }

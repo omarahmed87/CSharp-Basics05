1ee60c96599e4522f2dc0e2a30ca20311dee2a68
Omar Ahmed <cresssoomar@gmail.com>
2026-09-12 12:58:55 +0300

    3st answer

--- Basics 05/Program.cs
+++ Basics 05/Program.cs
@@ -1,7 +1,8 @@
-﻿#region 2st answer
+﻿#region 3st answer
 /* #region 1st answer
-/* 2-Create a 2x2 multidimensional array int[,] shelfCopies where shelf 0 has 3, 5 copies and
-shelf 1 has 1, 4 copies. Print the number of copies on shelf 1, slot 0.
+/* 3-Write a method called PrintWelcomeMessage that takes no parameters and prints
+"Welcome to the Library!". Call it from Main.
+
  */
 #endregion
 
@@ -10,10 +11,12 @@ public class Program
     public static void Main()
     {
 
-        int[,] shelfCopies = {
-            { 3,5 },
-            { 1,4 },
-        };
-        Console.WriteLine(shelfCopies[1, 0]);
+        PrintWelcomeMessage();
+
+    }
+
+    static void PrintWelcomeMessage()
+    {
+        Console.WriteLine("Welcome to the Library!");
     }
 }

6b19f7426c4ed476e593ac2b70c9301a2d6152c2
Omar Ahmed <cresssoomar@gmail.com>
2026-09-12 12:44:23 +0300

    1st answer

--- Basics 05/Program.cs
+++ Basics 05/Program.cs
@@ -1,1 +1,19 @@
-﻿Console.WriteLine("Hello, World!");
+﻿#region 1st answer
+/* 1-Create a one-dimensional array double[] prices with the values 25.5, 40.0, 33.75.
+ * Print the second price (index 1).
+ */
+#endregion
+
+public class Program
+{
+ public static void Main()
+    {
+
+        double[] prices = [25.5, 40.0, 33.75];
+        Console.WriteLine(prices[1]);
+
+    }
+}
+       
+
+   

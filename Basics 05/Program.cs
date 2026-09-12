16828d6a7f95a94b110b3bcae8008d0599308cfb
Omar Ahmed <cresssoomar@gmail.com>
2026-09-12 12:56:16 +0300

    2st answer

--- Basics 05/Program.cs
+++ Basics 05/Program.cs
@@ -1,19 +1,19 @@
-﻿#region 1st answer
-/* 1-Create a one-dimensional array double[] prices with the values 25.5, 40.0, 33.75.
- * Print the second price (index 1).
+﻿#region 2st answer
+/* #region 1st answer
+/* 2-Create a 2x2 multidimensional array int[,] shelfCopies where shelf 0 has 3, 5 copies and
+shelf 1 has 1, 4 copies. Print the number of copies on shelf 1, slot 0.
  */
 #endregion
 
 public class Program
 {
- public static void Main()
+    public static void Main()
     {
 
-        double[] prices = [25.5, 40.0, 33.75];
-        Console.WriteLine(prices[1]);
-
+        int[,] shelfCopies = {
+            { 3,5 },
+            { 1,4 },
+        };
+        Console.WriteLine(shelfCopies[1, 0]);
     }
 }
-       
-
-   
